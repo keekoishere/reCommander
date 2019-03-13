ccd135898c3ed24a1a2af7421c2d9d57526acc21
Francisco Esteves <35873909+keekoishere@users.noreply.github.com>
2019-03-13 16:21:57 +0000

    Update settings.py

--- recommander/settings.py
+++ recommander/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*ji7@2^g^0wz96)t0)oex-*@*l5_aluusruqy%+&y459#9f+u#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
